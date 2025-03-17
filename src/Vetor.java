870518c4f69b5e474f631290eed68a0bdbeb639e
Nicolas <nico124cunha@gmail.com>
2025-03-17 09:58:53 -0300

    finalizado o aumento da arraylist

--- src/Vetor.java
+++ src/Vetor.java
@@ -5,18 +5,46 @@ public class Vetor {
     private int totalDeAlunos = 0;
 
     public void adiciona(Aluno aluno){
-        // recebe um aluno
+        // recebe um aluno[
+        this.garanteEspaco();
         this.alunos[totalDeAlunos] = aluno;
         totalDeAlunos++;
     }
 
+    private boolean posicaoValida(int posicao){
+        return posicao>= 0 && posicao<= totalDeAlunos;
+    }
+
+    public void adiciona(int posicao, Aluno aluno){
+        this.garanteEspaco();
+        if (!posicaoValida(posicao)){
+            throw new IllegalArgumentException("posição onválida");
+        }
+        for (int i = totalDeAlunos - 1; i >= posicao ; i -= 1) {
+            alunos[i + 1] = alunos[i];
+        }
+        alunos[posicao] = aluno;
+        totalDeAlunos++;
+    }
+
+    private boolean posicaoOcupada(int posicao){
+        return posicao >= 0 && posicao < totalDeAlunos;
+    }
+
     public Aluno pega(int posicao){
         // recebe um aluno
-        return null;
+        if (!posicaoOcupada(posicao)){
+            throw new IllegalArgumentException("posição invalida");
+        }
+        return alunos[posicao];
     }
 
     public void remove(int posicao){
         // remove pela posição
+        for (int i = posicao; i < this.totalDeAlunos ; i++) {
+            this.alunos[i] = this.alunos[i + 1];
+        }
+        totalDeAlunos--;
     }
 
     public boolean contem(Aluno aluno){
@@ -38,5 +66,15 @@ public class Vetor {
     public String toString() {
         // facilitará na impressão
         return Arrays.toString(alunos);
+    }
+
+    private void garanteEspaco(){
+        if (totalDeAlunos == alunos.length){
+            Aluno[] novoArray = new Aluno[alunos.length * 2];
+            for (int i = 0; i < alunos.length; i++) {
+                novoArray[i] = alunos[i];
+            }
+            this.alunos = novoArray;
+        }
     }
 }
